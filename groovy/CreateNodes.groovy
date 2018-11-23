d07c0de2a94d985fd1a6cae3b48c46d0ad88e670
zodgevaibhav <zodgevaibhav@gmail.com>
2018-11-23 22:03:36 +0530

    Groovy script issue fix

--- groovy/CreateNodes.groovy
+++ groovy/CreateNodes.groovy
@@ -9,14 +9,15 @@ String agentExecutors = "1"
 
 agentList.eachLine
 {
-	DumbSlave slave = new DumbSlave(it,
-	agentDescription,
-	agentHome,
-	agentExecutors,
-	Mode.EXCLUSIVE,
-	"",
-	new JNLPLauncher(),
-	RetentionStratergy.INSTANCE)
+    DumbSlave slave = new DumbSlave(it,  // Agent name, usually matches the host computer's machine name
+            agentDescription,           // Agent description
+            agentHome,                  // Workspace on the agent's computer
+            agentExecutors,             // Number of executors
+            Mode.EXCLUSIVE,             // "Usage" field, EXCLUSIVE is "only tied to node", NORMAL is "any"
+            "",                         // Labels
+            new JNLPLauncher(),         // Launch strategy, JNLP is the Java Web Start setting services use
+            RetentionStrategy.INSTANCE) // Is the "Availability" field and INSTANCE means "Always"
+
+    Jenkins.instance.addNode(slave)
 	
-	Jenkins.instance.addNode(slave)
 }
--- groovy/java-setup.groovy
+++ groovy/java-setup.groovy
@@ -4,24 +4,20 @@ import hudson.tools.ZipExtractionInstaller
 
 def descriptor = new JDK.DescriptorImpl();
 
-new AntBuilder.copy(todir:'/var/jenkins_hime/plugins')
-{
-	fileset(dir:'/usr/share/jenkins/jenkins_home/plugins')
+
+def List<JDK> installations = []
+
+javaTools=[['name':'jdk8', 'url':'file:/var/jenkins_home/downloads/jdk-8u144-linux-x64.tar.gz', 'subdir':'jdk1.8.0_144'],
+      ['name':'jdk7', 'url':'file:/var/jenkins_home/downloads/jdk-7u80-linux-x64.tar.gz', 'subdir':'jdk1.7.0_80']]
+
+javaTools.each { javaTool ->
+
+    println("Setting up tool: ${javaTool.name}")
+
+    def installer = new ZipExtractionInstaller(javaTool.label as String, javaTool.url as String, javaTool.subdir as String);
+    def jdk = new JDK(javaTool.name as String, null, [new InstallSourceProperty([installer])])
+    installations.add(jdk)
+
 }
-
-def List<JDK> installation=[]
-
-javaTools=[['name':'jdk8','url':'file:/var/jenkins_home/downloads/jdk-8u172-linux-x64.tar.gz','subdir':'jdk1.8.0_131']]
-
-javaTools.each
-{
-	javaTool->
-		println("Setting up jdk:${javaTool.name}")
-		
-		def installer = new ZipExtractionInstaller(javaTool.label as String, javaTool.url as String, javaTool.subdir as String);
-		def jdk = new JDK(javaTool.name as String, null, [new InstallSourceProperty([installer])])
-		installation.add(jdk)
-}
-
-descriptor.setInstallations(installations.toArray(new JDK[installation.size()]))
+descriptor.setInstallations(installations.toArray(new JDK[installations.size()]))
 descriptor.save()
